e80a18344f875ee2d499cc03b517269fd3dd3c1b
babkamen <babkamen@gmail.com>
2017-05-13 16:49:15 +0300

    2 commit

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
 var express = require('express');
 var app = express();
-var moment = require('moment');
-
-app.get('/api/whoami', function(req, res) {
+var url='/api/whoami';
+app.get(url, function(req, res) {
 	var r = {
-		ipaddress: JSON.stringify(req.headers),
+		ipaddress: req.get("host"),
 		language: req.get("accept-language"),
-		software: "Windows NT 6.1; Win64; x64"
+		software: req.get("user-agent")
 	};
 	console.log(JSON.stringify(req.headers));
 	res.send(r);
 });
 
 app.get('/', function(req, res) {
-	res.sendFile(__dirname + '/index.html');
+		res.redirect(url);
 });
 
 app.set('port', (process.env.PORT || 3000));
